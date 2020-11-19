5eda9d798106eeb73b5783d783f206073204c6c9
davidy2911 <58455322+davidy2911@users.noreply.github.com>
2020-11-19 18:00:19 +0000

    Added main file and loss file
    Main file has specifics of the simple 2d example. Loss file has Loss class and loss_modifier function.

---
+++ loss.py
@@ -0,0 +1,52 @@
+import numpy as np
+import scipy as sp
+import sympy as sym
+
+a, b, x, y = sym.symbols('a b x y')
+
+class loss:
+    def __init__(self, E, data=[x,y]):
+        self.formula = E.subs([(x, data[0]), (y, data[1])])
+
+        self.gradient = np.array([[sym.diff(self.formula, a)], [sym.diff(self.formula, b)]])
+
+        self.global_minima = sym.solve(self.formula, b)[0]
+
+    def value(self, params):
+        return self.formula.subs([(a, params[0]),(b, params[1])])
+    
+    def plot(self, range_a, range_b):
+        sym.plotting.plot3d(self.formula, (a, range_a[0],range_a[1]), (b, range_b[0], range_b[1]))
+
+
+def loss_modifier(og_loss, h):
+    regulizer = (h/4)*(og_loss.gradient[0][0]**2 + og_loss.gradient[1][0]**2)
+    modified_formula = og_loss.formula + regulizer
+
+    return loss(modified_formula)
+        
+if __name__ == '__main__':
+    E = (y-a*b*x)**2/2
+    print(E)
+    
+    data = np.array([1, 0.6])
+    print(data)
+
+    og_loss = loss(E, data)
+
+    print(og_loss.formula)
+    print(og_loss.gradient)
+    
+    modified_loss = loss_modifier(og_loss, 0.025)
+
+    print(modified_loss.formula)
+    print(modified_loss.gradient)
+    
+    params = np.array([2.8, 3.5])
+    print(og_loss.value(params))
+    print(modified_loss.value(params))
+    
+    og_loss.plot((-5,5),(-5,5))
+    modified_loss.plot((-5,5),(-5,5))
+
+    print(og_loss.global_minima)
---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy as sp
+import sys
+import sympy as sym
+
+import loss as ls 
+
+a, b, x, y = sym.symbols('a b x y')
+
+E = ((y-a*b*x)**2)/2
+print("\n Formula for loss space for any data (x,y):")
+print (E)
+
+data = np.array([1,0.6])
+print("\nGiven data:")
+print(data)
+
+original_loss = ls.loss(E, data)
+
+print("\nFormula for original loss space for given data:")
+print(original_loss.formula)
+
+modified_loss = ls.loss_modifier(original_loss, 0.25)
+
+print("\nFormula for modified version of original loss space:")
+print(modified_loss.formula)
+
+range_a =(-5,5)
+range_b =(-5,5)
+
+print("\nPlot of original loss space over given range:\n")
+original_loss.plot(range_a, range_b)
+print("\nPlot of modified loss space over given range:\n")
+modified_loss.plot(range_a, range_b)
